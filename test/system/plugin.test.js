66a520194b5a5845bd338b74729f7ca1ef4f1d80
Anton Drukh <anton@snyk.io>
2018-08-05 20:37:19 +0300

    chore: eslint instead of jscs

--- test/system/plugin.test.js
+++ test/system/plugin.test.js
@@ -6,29 +6,29 @@ var plugin = require('../../lib');
 var subProcess = require('../../lib/sub-process');
 
 var rootNoWrapper = path.join(
-    __dirname, '..', 'fixtures', 'no-wrapper');
+  __dirname, '..', 'fixtures', 'no-wrapper');
 
 var rootWithWrapper = path.join(
-    __dirname, '..', 'fixtures', 'with-wrapper');
+  __dirname, '..', 'fixtures', 'with-wrapper');
 
 var subWithWrapper = path.join(
-    __dirname, '..', 'fixtures', 'with-wrapper-in-root');
+  __dirname, '..', 'fixtures', 'with-wrapper-in-root');
 
 test('run inspect()', function (t) {
   t.plan(1);
   return plugin.inspect('.', path.join(
     __dirname, '..', 'fixtures', 'no-wrapper', 'build.gradle'))
-  .then(function (result) {
-    t.equal(result.package
-      .dependencies['com.android.tools.build:builder']
-      .dependencies['com.android.tools:sdklib']
-      .dependencies['com.android.tools:repository']
-      .dependencies['com.android.tools:common']
-      .dependencies['com.android.tools:annotations'].version,
+    .then(function (result) {
+      t.equal(result.package
+        .dependencies['com.android.tools.build:builder']
+        .dependencies['com.android.tools:sdklib']
+        .dependencies['com.android.tools:repository']
+        .dependencies['com.android.tools:common']
+        .dependencies['com.android.tools:annotations'].version,
       '25.3.0',
       'correct version found');
-  })
-  .catch(t.fail);
+    })
+    .catch(t.fail);
 });
 
 test('windows without wrapper', function (t) {
@@ -38,11 +38,11 @@ test('windows without wrapper', function (t) {
   stubSubProcessExec(t);
 
   return plugin.inspect(rootNoWrapper, 'build.gradle')
-  .then(t.fail)
-  .catch(function () {
-    var cmd = subProcess.execute.getCall(0).args[0];
-    t.same(cmd, 'gradle', 'invokes gradle directly');
-  });
+    .then(t.fail)
+    .catch(function () {
+      var cmd = subProcess.execute.getCall(0).args[0];
+      t.same(cmd, 'gradle', 'invokes gradle directly');
+    });
 });
 
 test('darwin without wrapper', function (t) {
@@ -52,11 +52,11 @@ test('darwin without wrapper', function (t) {
   stubSubProcessExec(t);
 
   return plugin.inspect(rootNoWrapper, 'build.gradle')
-  .then(t.fail)
-  .catch(function () {
-    var cmd = subProcess.execute.getCall(0).args[0];
-    t.same(cmd, 'gradle', 'invokes gradle directly');
-  });
+    .then(t.fail)
+    .catch(function () {
+      var cmd = subProcess.execute.getCall(0).args[0];
+      t.same(cmd, 'gradle', 'invokes gradle directly');
+    });
 });
 
 test('windows with wrapper', function (t) {
@@ -66,13 +66,13 @@ test('windows with wrapper', function (t) {
   stubSubProcessExec(t);
 
   return plugin.inspect(rootWithWrapper, 'build.gradle')
-  .then(t.fail)
-  .catch(function () {
-    var cmd = subProcess.execute.getCall(0).args[0];
-    var expectedCmd = path.join(
-      __dirname, '..', 'fixtures', 'with-wrapper', 'gradlew.bat');
-    t.same(cmd, expectedCmd, 'invokes wrapper bat');
-  });
+    .then(t.fail)
+    .catch(function () {
+      var cmd = subProcess.execute.getCall(0).args[0];
+      var expectedCmd = path.join(
+        __dirname, '..', 'fixtures', 'with-wrapper', 'gradlew.bat');
+      t.same(cmd, expectedCmd, 'invokes wrapper bat');
+    });
 });
 
 test('darwin with wrapper', function (t) {
@@ -82,13 +82,13 @@ test('darwin with wrapper', function (t) {
   stubSubProcessExec(t);
 
   return plugin.inspect(rootWithWrapper, 'build.gradle')
-  .then(t.fail)
-  .catch(function () {
-    var cmd = subProcess.execute.getCall(0).args[0];
-    var expectedCmd = path.join(
-      __dirname, '..', 'fixtures', 'with-wrapper', 'gradlew');
-    t.same(cmd, expectedCmd, 'invokes wrapper script');
-  });
+    .then(t.fail)
+    .catch(function () {
+      var cmd = subProcess.execute.getCall(0).args[0];
+      var expectedCmd = path.join(
+        __dirname, '..', 'fixtures', 'with-wrapper', 'gradlew');
+      t.same(cmd, expectedCmd, 'invokes wrapper script');
+    });
 });
 
 test('windows with wrapper in root', function (t) {
@@ -98,13 +98,13 @@ test('windows with wrapper in root', function (t) {
   stubSubProcessExec(t);
 
   return plugin.inspect(subWithWrapper, path.join('app', 'build.gradle'))
-  .then(t.fail)
-  .catch(function () {
-    var cmd = subProcess.execute.getCall(0).args[0];
-    var expectedCmd = path.join(
-      __dirname, '..', 'fixtures', 'with-wrapper-in-root', 'gradlew.bat');
-    t.same(cmd, expectedCmd, 'invokes wrapper bat');
-  });
+    .then(t.fail)
+    .catch(function () {
+      var cmd = subProcess.execute.getCall(0).args[0];
+      var expectedCmd = path.join(
+        __dirname, '..', 'fixtures', 'with-wrapper-in-root', 'gradlew.bat');
+      t.same(cmd, expectedCmd, 'invokes wrapper bat');
+    });
 });
 
 test('darwin with wrapper in root', function (t) {
@@ -114,27 +114,27 @@ test('darwin with wrapper in root', function (t) {
   stubSubProcessExec(t);
 
   return plugin.inspect(subWithWrapper, path.join('app', 'build.gradle'))
-  .then(t.fail)
-  .catch(function () {
-    var cmd = subProcess.execute.getCall(0).args[0];
-    var expectedCmd = path.join(
-      __dirname, '..', 'fixtures', 'with-wrapper-in-root', 'gradlew');
-    t.same(cmd, expectedCmd, 'invokes wrapper script');
-  });
+    .then(t.fail)
+    .catch(function () {
+      var cmd = subProcess.execute.getCall(0).args[0];
+      var expectedCmd = path.join(
+        __dirname, '..', 'fixtures', 'with-wrapper-in-root', 'gradlew');
+      t.same(cmd, expectedCmd, 'invokes wrapper script');
+    });
 });
 
 function stubPlatform(platform, t) {
   sinon.stub(os, 'platform')
-  .callsFake(function () {
-    return platform;
-  });
+    .callsFake(function () {
+      return platform;
+    });
   t.teardown(os.platform.restore);
 }
 
 function stubSubProcessExec(t) {
   sinon.stub(subProcess, 'execute')
-  .callsFake(function () {
-    return Promise.reject('abort');
-  });
+    .callsFake(function () {
+      return Promise.reject('abort');
+    });
   t.teardown(subProcess.execute.restore);
 }
