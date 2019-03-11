04cf66ddfa4cd484e609836ee5f10f2ae95daefb
Konstantin Yegupov <konstantin@snyk.io>
2019-03-11 17:19:09 +0000

    feat: deps json generated in Gradle code, configurations merged

--- README.md
+++ README.md
@@ -7,3 +7,17 @@ Snyk helps you find, fix and monitor for known vulnerabilities in your dependenc
 ## Snyk Gradle CLI Plugin
 
 This plugin provides dependency metadata for Gradle projects that use `gradle` and have a `build.gradle` file.
+
+Supported Snyk command line arguments:
+
+* `--gradle-sub-project=foo` return dependencies for a specific subproject (by default, return only the
+  dependencies for the top-level project)
+
+Additional command line arguments to Gradle can be provided after `--`, for example:
+
+* `-- --configuration=foo` only fetch dependencies for a certain configuration (by default, merged deps for
+  all the configurations are returned).
+
+## Under the hood
+
+See `lib/init.gradle` for the Groovy script injected in Gradle builds to gather and resolve the dependencies.
--- test/system/plugin.test.js
+++ test/system/plugin.test.js
@@ -137,9 +137,9 @@ test('darwin with wrapper in root', function (t) {
     });
 });
 
-test('only sub-project has deps', function (t) {
+test('multi-project: only sub-project has deps and they are returned', function (t) {
   t.plan(2);
-  const options = {
+  var options = {
     'gradle-sub-project': 'subproj',
   };
   return plugin.inspect('.',
@@ -159,6 +159,94 @@ test('only sub-project has deps', function (t) {
       'correct version found');
     })
     .catch(t.threw);
+});
+
+test('multi-project: only sub-project has deps, none returned for main', function (t) {
+  t.plan(2);
+  var resultPromise = plugin.inspect('.',
+    path.join(__dirname, '..', 'fixtures', 'multi-project', 'build.gradle'));
+  resultPromise.then(function (result) {
+    t.match(result.package.name, '.',
+      'returned project name is not sub-project');
+    t.notOk(result.package.dependencies);
+  })
+    .catch(t.threw);
+});
+
+test('multi-confg: both compile and runtime deps picked up by default', function (t) {
+  t.plan(4);
+  var resultPromise = plugin.inspect('.',
+    path.join(__dirname, '..', 'fixtures', 'multi-config', 'build.gradle'));
+  resultPromise.then(function (result) {
+    t.match(result.package.name, '.',
+      'returned project name is not sub-project');
+    t.equal(result.package
+      .dependencies['com.android.tools.build:builder']
+      .dependencies['com.android.tools:sdklib']
+      .dependencies['com.android.tools:repository']
+      .dependencies['com.android.tools:common']
+      .dependencies['com.android.tools:annotations'].version,
+    '25.3.0',
+    'correct version of compile+runtime dep found');
+    t.equal(result.package
+      .dependencies['javax.servlet:servlet-api'].version,
+    '2.5',
+    'correct version of compileOnly dep found');
+    t.equal(Object.keys(result.package.dependencies).length, 6, 'top level deps: 6');
+  })
+    .catch(t.threw);
+});
+
+test('multi-confg: only deps for specified conf are picked up', function (t) {
+  t.plan(4);
+  var resultPromise = plugin.inspect('.',
+    path.join(__dirname, '..', 'fixtures', 'multi-config', 'build.gradle'),
+    {args: ['--configuration', 'compileOnly']});
+  resultPromise.then(function (result) {
+    t.match(result.package.name, '.',
+      'returned project name is not sub-project');
+    t.notOk(result.package
+      .dependencies['com.android.tools.build:builder'],
+    'no compile+runtime dep found');
+    t.equal(result.package
+      .dependencies['javax.servlet:servlet-api'].version,
+    '2.5',
+    'correct version of compileOnly dep found');
+    t.equal(Object.keys(result.package.dependencies).length, 1, 'top level deps: 1');
+  })
+    .catch(t.threw);
+});
+
+// Timeout is 150 seconds because Gradle .kts builds are slower than usual
+test('build.gradle.kts files are supported', {timeout: 150000}, function (t) {
+  t.plan(3);
+  var resultPromise = plugin.inspect('.',
+    path.join(__dirname, '..', 'fixtures', 'gradle-kts', 'build.gradle.kts'));
+  resultPromise.then(function (result) {
+    t.match(result.package.name, '.',
+      'returned project name is not sub-project');
+    t.equal(result.package
+      .dependencies['org.jetbrains.kotlin:kotlin-stdlib-jdk8']
+      .dependencies['org.jetbrains.kotlin:kotlin-stdlib']
+      .dependencies['org.jetbrains.kotlin:kotlin-stdlib-common'].version,
+    '1.3.21',
+    'correct version of a dependency is found');
+    t.equal(Object.keys(result.package.dependencies).length, 6, 'top level deps: 6');
+  })
+    .catch(t.threw);
+});
+
+test('malformed build.gradle', function (t) {
+  t.plan(1);
+  var resultPromise = plugin.inspect('.',
+    path.join(__dirname, '..', 'fixtures', 'malformed-build-gradle', 'build.gradle'),
+    {args: ['--configuration', 'compileOnly']});
+  resultPromise.then(function success(result) {
+    console.log(result);
+    t.fail('expected inspect to fail');
+  }, function failure(err) {
+    t.match(err.toString(), /unexpected token/, 'error thrown as expected');
+  });
 });
 
 function stubPlatform(platform, t) {
