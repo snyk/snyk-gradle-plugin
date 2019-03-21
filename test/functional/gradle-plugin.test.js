2eb715f39926a9937bbc9bbbd865433222039d99
kyegupov <kyegupov4@gmail.com>
2019-03-21 16:24:27 +0000

    Revert "refactor: convert to Typescript (no proper lint, no types yet)"

--- test/functional/gradle-plugin.test.js
+++ test/functional/gradle-plugin.test.js
@@ -1,5 +1,5 @@
-import {test} from 'tap';
-import {__tests as plugin} from '../../lib';
+var test = require('tap').test;
+var plugin = require('../../lib').__tests;
 
 test('check build args with array', function (t) {
   t.plan(1);
@@ -36,11 +36,8 @@ test('check build args with string', function (t) {
 
 test('extractJsonFromScriptOutput', function (t) {
   t.plan(1);
-  var result = plugin.extractJsonFromScriptOutput(`Mr Gradle says hello
-la dee da, la dee da
-JSONDEPS {"hello": "world"}
-some other noise`);
-  t.deepEqual(result, {hello: 'world'});
+  var result = plugin.extractJsonFromScriptOutput('Mr Gradle says hello\nla dee da, la dee da\nJSONDEPS {"hello": "world"}\nsome other noise');
+  t.deepEqual(result, {'hello': 'world'});
   t.end();
 });
 
