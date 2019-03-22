a2fe260f7d53e51e00a751c9f6278e0e3ae62780
kyegupov <kyegupov4@gmail.com>
2019-03-22 17:12:27 +0000

    fix: correctly handle scanning of .gradle file for a subproject (#39)
    * fix: correctly handle targeting a subproject .gradle file

--- test/system/plugin.test.js
+++ test/system/plugin.test.js
@@ -249,6 +249,68 @@ test('malformed build.gradle', function (t) {
   });
 });
 
+test('multi-project, explicitly targeting a subproject build file', function (t) {
+  t.plan(2);
+  return plugin.inspect('.',
+    path.join(__dirname, '..', 'fixtures', 'multi-project', 'subproj', 'build.gradle'))
+    .then(function (result) {
+      t.equals(result.package.name, '.',
+        'root project is "."');
+
+      t.equal(result.package
+        .dependencies['com.android.tools.build:builder']
+        .dependencies['com.android.tools:sdklib']
+        .dependencies['com.android.tools:repository']
+        .dependencies['com.android.tools:common']
+        .dependencies['com.android.tools:annotations'].version,
+      '25.3.0',
+      'correct version found');
+    })
+    .catch(t.threw);
+});
+
+test('multi-project, ran from root, targeting subproj', function (t) {
+  t.plan(2);
+  return plugin.inspect(
+    path.join(__dirname, '..', 'fixtures', 'multi-project'),
+    'subproj/build.gradle')
+    .then(function (result) {
+      t.equals(result.package.name, 'multi-project',
+        'root project is "multi-project"');
+
+      t.equal(result.package
+        .dependencies['com.android.tools.build:builder']
+        .dependencies['com.android.tools:sdklib']
+        .dependencies['com.android.tools:repository']
+        .dependencies['com.android.tools:common']
+        .dependencies['com.android.tools:annotations'].version,
+      '25.3.0',
+      'correct version found');
+    })
+    .catch(t.threw);
+});
+
+test('multi-project, ran from a subproject directory', function (t) {
+  t.plan(2);
+  return plugin.inspect(
+    path.join(__dirname, '..', 'fixtures', 'multi-project', 'subproj'),
+    'build.gradle')
+    .then(function (result) {
+      t.equals(result.package.name, 'subproj',
+        'root project is "subproj"');
+
+      t.equal(result.package
+        .dependencies['com.android.tools.build:builder']
+        .dependencies['com.android.tools:sdklib']
+        .dependencies['com.android.tools:repository']
+        .dependencies['com.android.tools:common']
+        .dependencies['com.android.tools:annotations'].version,
+      '25.3.0',
+      'correct version found');
+    })
+    .catch(t.threw);
+});
+
 function stubPlatform(platform, t) {
   sinon.stub(os, 'platform')
     .callsFake(function () {
