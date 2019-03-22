13294b377a69f5e7239b6843e74f8e03dfbee3a1
Konstantin Yegupov <konstantin@snyk.io>
2019-03-22 14:19:40 +0000

    fix: correctly handle scanning of .gradle file for a subproject

--- test/system/plugin.test.js
+++ test/system/plugin.test.js
@@ -249,6 +249,26 @@ test('malformed build.gradle', function (t) {
   });
 });
 
+test('multi-project, explicitly targeting a subproject build file', function (t) {
+  t.plan(2);
+  return plugin.inspect('.',
+    path.join(__dirname, '..', 'fixtures', 'multi-project', 'subproj', 'build.gradle'))
+    .then(function (result) {
+      t.equals(result.package.name, '.',
+        'root project is "."');
+
+      t.equal(result.package
+        .dependencies['com.android.tools.build:builder']
+        .dependencies['com.android.tools:sdklib']
+        .dependencies['com.android.tools:repository']
+        .dependencies['com.android.tools:common']
+        .dependencies['com.android.tools:annotations'].version,
+      '25.3.0',
+      'correct version found');
+    })
+    .catch(t.threw);
+});
+
 function stubPlatform(platform, t) {
   sinon.stub(os, 'platform')
     .callsFake(function () {
