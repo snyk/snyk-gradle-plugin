0f7c50e3b1b03e06e7987ea7f03fb689341e201b
Lili Kastilio <lili@lilianakastilio.co.uk>
2019-04-01 18:14:21 +0100

    Revert "Test/ts node" (#50)

--- test/common.ts
+++ test/common.ts
@@ -5,7 +5,7 @@ import * as subProcess from '../lib/sub-process';
 
 export function fixtureDir(f: string): string {
   // Assuming current directory is ./dist/test
-  return path.join(__dirname, './fixtures', f);
+  return path.join(__dirname, '../../test/fixtures', f);
 }
 
 export function stubPlatform(platform, t) {
