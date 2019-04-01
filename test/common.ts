43e2cdb35ac61d48cf5676468e4df6f41e84e729
Lili Kastilio <lili@lilianakastilio.co.uk>
2019-04-01 16:07:36 +0100

    Revert "Test/ts node"

--- test/common.ts
+++ test/common.ts
@@ -2,5 +2,5 @@ import * as path from 'path';
 
 export function fixtureDir(f: string): string {
   // Assuming current directory is ./dist/test
-  return path.join(__dirname, './fixtures', f);
+  return path.join(__dirname, '../../test/fixtures', f);
 }
