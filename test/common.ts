bb0825e2f56456a03cd6a67f35213848ffe29bfb
Shaun Smith <shaun@snyk.io>
2019-04-01 18:35:00 +0100

    chore: switch back to non-forked node-tap
    - node-tap has added typescript/ts-node support, so let's use it

--- test/common.ts
+++ test/common.ts
@@ -4,8 +4,8 @@ import {stub, SinonStub} from 'sinon';
 import * as subProcess from '../lib/sub-process';
 
 export function fixtureDir(f: string): string {
-  // Assuming current directory is ./dist/test
-  return path.join(__dirname, '../../test/fixtures', f);
+  // Assuming current directory is ./test
+  return path.join(__dirname, 'fixtures', f);
 }
 
 export function stubPlatform(platform, t) {
