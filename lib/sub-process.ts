1412857c9b6300f9d55720b6ca43fb2efe6f15f3
Konstantin Yegupov <konstantin@snyk.io>
2019-05-23 17:44:59 +0100

    feat: add debugging output for init script

--- lib/sub-process.ts
+++ lib/sub-process.ts
@@ -1,6 +1,15 @@
 import * as childProcess from 'child_process';
+import debugModule = require('debug');
 
-export function execute(command: string, args: string[], options: {cwd?: string}): Promise<string> {
+const debugLogging = debugModule('snyk-gradle-plugin');
+
+// Executes a subprocess. Resolves successfully with stdout contents if the exit code is 0.
+export function execute(
+  command: string,
+  args: string[],
+  options: {cwd?: string},
+  perLineCallback?: (s: string) => Promise<void>,
+): Promise<string> {
   const spawnOptions: childProcess.SpawnOptions = {shell: true};
   if (options && options.cwd) {
     spawnOptions.cwd = options.cwd;
@@ -11,10 +20,14 @@ export function execute(command: string, args: string[], options: {cwd?: string}
     let stderr = '';
 
     const proc = childProcess.spawn(command, args, spawnOptions);
-    proc.stdout.on('data', (data) => {
-      stdout = stdout + data;
+    proc.stdout.on('data', (data: Buffer) => {
+      const strData = data.toString();
+      stdout = stdout + strData;
+      if (perLineCallback) {
+        strData.split('\n').forEach(perLineCallback);
+      }
     });
-    proc.stderr.on('data', (data) => {
+    proc.stderr.on('data', (data: Buffer) => {
       stderr = stderr + data;
     });
 
@@ -30,7 +43,11 @@ ${stdout}
 ${stderr}
 `));
       }
-      resolve(stdout || stderr);
+      if (stderr) {
+        debugLogging('subprocess exit code = 0, but stderr was not empty: ' + stderr);
+      }
+
+      resolve(stdout);
     });
   });
 }
