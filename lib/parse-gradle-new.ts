dcd6629b43424ff97479be779d84b2aef95a178d
aviad2020 <aviadhahami@gmail.com>
2020-02-25 19:20:18 +0200

    feat: parser

--- lib/parse-gradle-new.ts
+++ lib/parse-gradle-new.ts
@@ -3,6 +3,7 @@ import {DepTree} from '@snyk/cli-interface/legacy/common';
 
 const LOG_LABEL = /^SNYKDEPS\s*/gm;
 const DIGRAPH_SEGMENT = /digraph([\s\S]*?)}/g;
+const GRADLE_DEP = /(\S+):(\S+):(\S+):(\S+):(\S+)/gmi;
 const BASE_PROJECT = {
     name: 'no-name',
     version: '0.0.0',
@@ -12,10 +13,22 @@ const BASE_PROJECT = {
 
 function extractProject(projectString: string): DepTree {
 
+    // 0 -> package name
+    // 1 -> also package name?
+    // 2 -> just "jar" string
+    // 3 -> version
+    // 4 -> just "runtime" string
+    const projectSplit: string[] = projectString.split(GRADLE_DEP);
+    const packageName: string = `${projectSplit[0]}:${projectSplit[1]}`;
+    const packageVersion: string = projectSplit[3];
+
 }
 
 function parseGradle(gradleOutput: string, withDev: boolean): DepTree {
-    const cleanOutput: string = gradleOutput.replace(LOG_LABEL, '');
+    const cleanOutput: string = gradleOutput
+        .replace(LOG_LABEL, '')
+        .replace('"', '');
+
     const projectsSplit: string[] = cleanOutput.split(DIGRAPH_SEGMENT);
     if (!projectsSplit.length) {
         throw Error('No projects found');
