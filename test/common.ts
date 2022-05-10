12a9f85aca5fd4b5f9c29fb08746b0a3cb937c73
magdziarek <ola.magdziarek@snyk.io>
2022-05-10 15:56:52 +0200

    remove redundant functions

--- test/common.ts
+++ test/common.ts
@@ -1,24 +1,15 @@
 import * as path from 'path';
 import * as os from 'os';
 import { stub, SinonStub } from 'sinon';
-import * as subProcess from '../lib/sub-process';
-import { Test } from 'tap';
 
 export function fixtureDir(f: string): string {
   // Assuming current directory is ./test
   return path.join(__dirname, 'fixtures', f);
 }
 
-export function stubPlatform(platform: string, t: Test) {
+export function stubPlatform(platform: string) {
   stub(os, 'platform').callsFake(() => {
     return platform;
   });
-  t.teardown((os.platform as SinonStub).restore);
-}
-
-export function stubSubProcessExec(t: Test) {
-  stub(subProcess, 'execute').callsFake(() => {
-    return Promise.reject(new Error('fake process aborted'));
-  });
-  t.teardown((subProcess.execute as SinonStub).restore);
+  (os.platform as SinonStub).restore;
 }
