75c8f0edf61a7aaf8a328f51af04c984d2b960ed
magdziarek <ola.magdziarek@snyk.io>
2022-06-10 18:17:23 +0200

    chore: change jest config file to typescript
    TARDIS-911

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from '@jest/types';
+
+export default async (): Promise<Config.InitialOptions> => {
+  return {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    transform: {}, // ignore .babelrc file
+    collectCoverage: false, // Enabled by running `npm run test:coverage`
+    collectCoverageFrom: ['src/**/*.ts'],
+    coverageReporters: ['text-summary', 'html'],
+    testMatch: ['<rootDir>/test/**/*.spec.ts', '<rootDir>/test/**/*.test.ts'],
+    globals: {
+      'ts-jest': {
+        tsconfig: '<rootDir>/test/manual/tsconfig.json',
+      },
+    },
+    modulePathIgnorePatterns: ['<rootDir>/test/.*fixtures'],
+  };
+};
