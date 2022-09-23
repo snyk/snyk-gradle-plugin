cad5070199446ca73c4e8ecaa5e97b2519f3a0b5
jdunsby <johnny.dunsby@snyk.io>
2022-09-23 16:47:27 +0100

    feat: implement gradle-normalize-deps hash searching

--- test/common.ts
+++ test/common.ts
@@ -1,7 +1,5 @@
 import * as path from 'path';
-import * as os from 'os';
 import * as fs from 'fs';
-import { stub, SinonStub } from 'sinon';
 import { DepGraphData } from '@snyk/dep-graph';
 
 export function fixtureDir(f: string): string {
@@ -12,15 +10,6 @@ export function fixtureDir(f: string): string {
 export function getPathToFixture(f: string = ''): string {
   // Assuming current directory is ./test
   return path.join(__dirname, 'fixtures-with-wrappers', f);
-}
-
-export function stubPlatform(platform: string) {
-  stub(os, 'platform').callsFake(() => {
-    return platform;
-  });
-  return function restorePlatform() {
-    (os.platform as SinonStub).restore();
-  };
 }
 
 // save a depGraph to a dep-graph.json file
--- test/system/fixtures-with-wrappers.test.ts
+++ test/system/fixtures-with-wrappers.test.ts
@@ -1,8 +1,21 @@
 import * as path from 'path';
 import * as fs from 'fs';
+import { createFromJSON } from '@snyk/dep-graph';
+
 import { getPathToFixture } from '../common';
 import { inspect } from '../../lib';
-import { createFromJSON } from '@snyk/dep-graph';
+import type { PomCoords } from '../../lib/types';
+
+jest.mock('../../lib/index.ts', () => {
+  return {
+    __esModule: true,
+    ...jest.requireActual('../../lib/index.ts'),
+    getMavenPackageInfo: jest.fn(
+      (_, depCoords: PomCoords) =>
+        `${depCoords.groupId}:${depCoords.artifactId}@${depCoords.version}`,
+    ),
+  };
+});
 
 // specify fixtures to test, or leave empty to test all fixtures
 let fixtures: string[] = [];
@@ -10,6 +23,10 @@ let fixtures: string[] = [];
 if (!fixtures.length) fixtures = fs.readdirSync(getPathToFixture());
 
 describe('inspect() fixtures', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
   fixtures.forEach((fixtureName) => {
     const isKotlin = fixtureName.startsWith('kts');
     const dsl = isKotlin ? 'Kotlin' : 'Groovy';
@@ -27,6 +44,28 @@ describe('inspect() fixtures', () => {
         expectedDepGraph.equals(result.dependencyGraph);
 
       expect(resultMatchesExpected).toBeTruthy();
-    });
+    }, 100000);
+  });
+
+  fixtures.forEach((fixtureName) => {
+    const isKotlin = fixtureName.startsWith('kts');
+    const dsl = isKotlin ? 'Kotlin' : 'Groovy';
+    test(`${dsl} fixture: ${fixtureName} - gradleNormalizeDeps`, async () => {
+      const fixturePath = getPathToFixture(fixtureName);
+      const buildFileName = isKotlin ? 'build.gradle.kts' : 'build.gradle';
+      const pathToBuildConfig = path.join(fixturePath, buildFileName);
+      const expectedDepGraphJson = require(`${fixturePath}/dep-graph.json`);
+      const expectedDepGraph = createFromJSON(expectedDepGraphJson);
+
+      const result = await inspect('.', pathToBuildConfig, {
+        gradleNormalizeDeps: true,
+      });
+
+      const resultMatchesExpected =
+        result.dependencyGraph &&
+        expectedDepGraph.equals(result.dependencyGraph);
+
+      expect(resultMatchesExpected).toBeTruthy();
+    }, 100000);
   });
 });
