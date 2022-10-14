f115e10c069f3bf404f164e9f88ba20a2a09a185
ola magdziarek <ola.magdziarek@snyk.io>
2022-10-14 09:10:36 +0200

    fix: identify projects by path (#234)

---
+++ test/fixtures/subprojects-same-name/README.md
@@ -0,0 +1,3 @@
+# Multi-module project with a nested subproject with the same name as a subproject in root
+
+:greeter:subproj is dependent on :subproj
--- test/manual/gradle-stdout.spec.ts
+++ test/manual/gradle-stdout.spec.ts
@@ -15,6 +15,7 @@ describe('findProjectsInExtractedJSON', () => {
     async ({ rootDir, targetFile }) => {
       const jsonExtractedFromGradleStdout = {
         defaultProject: 'tardis-master',
+        defaultProjectKey: 'tardis-master',
         projects: {
           'tardis-master': {
             targetFile,
