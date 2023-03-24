f827b5abc5028a33df2b3f971f4f4c5c283c1de7
Jason Luong <jason.luong@snyk.io>
2023-03-24 14:22:44 +0000

    fix: bug in the CLI where envVars were not being restored correctly

--- lib/sub-process.ts
+++ lib/sub-process.ts
@@ -8,14 +8,30 @@ const debugLogging = debugModule('snyk-gradle-plugin');
 export function execute(
   command: string,
   args: string[],
-  options: { cwd?: string },
+  options: { cwd?: string, env?: NodeJS.ProcessEnv },
   perLineCallback?: (s: string) => Promise<void>,
 ): Promise<string> {
-  const spawnOptions: childProcess.SpawnOptions = { shell: true };
-  if (options && options.cwd) {
+  const spawnOptions: childProcess.SpawnOptions = { shell: true, env: { ...process.env } };
+  if (options?.cwd) {
     spawnOptions.cwd = options.cwd;
   }
+  if (options?.env) {
+    spawnOptions.env = { ...process.env, ...options.env };
+  }
+
   args = quoteAll(args, spawnOptions);
+
+  // Before spawning an external process, we look if we need to restore the system proxy configuration,
+  // which overides the cli internal proxy configuration.
+  if (process.env.SNYK_SYSTEM_HTTP_PROXY !== undefined) {
+    spawnOptions.env.HTTP_PROXY = process.env.SNYK_SYSTEM_HTTP_PROXY;
+  }
+  if (process.env.SNYK_SYSTEM_HTTPS_PROXY !== undefined) {
+    spawnOptions.env.HTTPS_PROXY = process.env.SNYK_SYSTEM_HTTPS_PROXY;
+  }
+  if (process.env.SNYK_SYSTEM_NO_PROXY !== undefined) {
+    spawnOptions.env.NO_PROXY = process.env.SNYK_SYSTEM_NO_PROXY;
+  }
 
   return new Promise((resolve, reject) => {
     let stdout = '';
