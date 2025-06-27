a84bfc798f64f4d985fb148bca14de25248d184c
Calum Harrison <calum.harrison@snyk.io>
2025-06-27 16:28:27 +0100

    fix: Precomputes child links and uses set for ancestry checks

--- test/functional/graph.spec.ts
+++ test/functional/graph.spec.ts
@@ -1,5 +1,5 @@
 import { DepGraphBuilder } from '@snyk/dep-graph';
-import { buildGraph, findChildren } from '../../lib/graph';
+import { buildGraph } from '../../lib/graph';
 
 describe('buildGraph', () => {
   it('returns empty when graph empty', async () => {
@@ -293,72 +293,5 @@ describe('buildGraph', () => {
     expect(expectNoLabel).toContainEqual({
       info: {},
     });
-  });
-});
-
-describe('findChildren', () => {
-  it('returns empty when graph empty', () => {
-    const received = findChildren('root-node', [], {});
-    expect(received).toEqual([]);
-  });
-
-  it('returns empty when graph has no parent id', () => {
-    const received = findChildren('not-found', [], {
-      'a@1': {
-        name: 'a',
-        version: '1',
-        parentIds: ['root-node'],
-      },
-    });
-    expect(received).toEqual([]);
-  });
-
-  it('returns nodes with given parent id', () => {
-    const received = findChildren('root-node', [], {
-      'a@1': {
-        name: 'a',
-        version: '1',
-        parentIds: ['root-node'],
-      },
-      'b@1': {
-        name: 'b',
-        version: '1',
-        parentIds: ['a@1'],
-      },
-      'c@1': {
-        name: 'c',
-        version: '1',
-        parentIds: ['root-node'],
-      },
-    });
-    expect(received).toEqual([
-      { id: 'a@1', ancestry: [], parentId: 'root-node' },
-      { id: 'c@1', ancestry: [], parentId: 'root-node' },
-    ]);
-  });
-
-  it('returns nodes with given parent id when multiple parents', () => {
-    const received = findChildren('root-node', [], {
-      'a@1': {
-        name: 'a',
-        version: '1',
-        parentIds: ['root-node'],
-      },
-      'b@1': {
-        name: 'b',
-        version: '1',
-        parentIds: ['root-node', 'a@1'],
-      },
-      'c@1': {
-        name: 'c',
-        version: '1',
-        parentIds: ['root-node'],
-      },
-    });
-    expect(received).toEqual([
-      { id: 'a@1', ancestry: [], parentId: 'root-node' },
-      { id: 'b@1', ancestry: [], parentId: 'root-node' },
-      { id: 'c@1', ancestry: [], parentId: 'root-node' },
-    ]);
   });
 });
