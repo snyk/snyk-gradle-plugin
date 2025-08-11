2e3780ca4fde328a372924d4ea08debe904d2d80
Evgenii Kotelnikov <evgenii.kotelnikov@snyk.io>
2025-08-11 13:54:16 +0200

    feat: add support for Gradle 9.0.0

---
+++ test/fixtures/gradle-kts-gradle9/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.2.0"
+    kotlin("plugin.spring") version "2.2.0"
+    kotlin("plugin.jpa") version "2.2.0"
+}
+
+version = "1.0.0-SNAPSHOT"
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.jetbrains.kotlin:kotlin-reflect") {
+        exclude(module = "junit")
+    }
+}
--- test/system/kotlin.test.ts
+++ test/system/kotlin.test.ts
@@ -3,19 +3,24 @@ import { fixtureDir } from '../common';
 import { inspect } from '../../lib';
 
 const gradleVersionFromProcess = process.env.GRADLE_VERSION || '';
-const gradleVersionInUse: number = parseInt(
-  gradleVersionFromProcess.split('.')[0],
-);
-const isKotlinSupported: boolean = gradleVersionInUse > 4 ? true : false;
-const kotlinVersion = gradleVersionInUse < 7 ? '1.3.21' : '1.8.10';
+const gradleVersionInUse: number =
+  parseInt(gradleVersionFromProcess.split('.')[0]) || 0;
+const kotlinVersion =
+  gradleVersionInUse < 7
+    ? '1.3.21'
+    : gradleVersionInUse < 9
+    ? '1.8.10'
+    : '2.2.0';
 const classifierMap: Record<number, string> = {
   7: ':gradle71',
   8: ':gradle76',
 };
 
-// Gradle .kts builds are slower than usual so timeout is set to 150 sec in package.json
-if (isKotlinSupported) {
-  test('build.gradle.kts files are supported with Gradle version > 5', async () => {
+if (gradleVersionInUse < 5) {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  test('build.gradle.kts are not supported with Gradle version < 5', () => {});
+} else if (gradleVersionInUse < 9) {
+  test('build.gradle.kts files are supported with Gradle version 5, 6, 7, 8', async () => {
     const result = await inspect(
       '.',
       path.join(fixtureDir('gradle-kts'), 'build.gradle.kts'),
@@ -59,6 +64,54 @@ if (isKotlinSupported) {
     });
   }, 200000);
 } else {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  test('build.gradle.kts are not supported with Gradle version < 5', () => {});
+  test('build.gradle.kts files are supported with Gradle version >= 9', async () => {
+    const result = await inspect(
+      '.',
+      path.join(fixtureDir('gradle-kts-gradle9'), 'build.gradle.kts'),
+    );
+    expect(result.dependencyGraph?.rootPkg.name).toMatch('gradle-kts-gradle9');
+    expect(result.meta?.gradleProjectName).toMatch('gradle-kts-gradle9');
+    const pkgs = result.dependencyGraph?.getDepPkgs() || [];
+    const nodeIds: string[] = [];
+    Object.keys(pkgs).forEach((id) => {
+      nodeIds.push(`${pkgs[id].name}@${pkgs[id].version}`);
+    });
+
+    const expectedNodeId = `org.jetbrains.kotlin:kotlin-stdlib@${kotlinVersion}`;
+    expect(nodeIds).toContain(expectedNodeId);
+
+    // double parsing to have access to internal depGraph data, no methods available to properly
+    // return the deps nodeIds list that belongs to a node
+    const graphObject: any = JSON.parse(JSON.stringify(result.dependencyGraph));
+    const { deps: directDependencies } = graphObject.graph.nodes[0];
+    const expectedDirectDependencies = [
+      {
+        nodeId: `org.jetbrains.kotlin:kotlin-stdlib-jdk8:jar@${kotlinVersion}`,
+      },
+      {
+        nodeId: `org.jetbrains.kotlin:kotlin-build-tools-impl:jar@${kotlinVersion}`,
+      },
+      {
+        nodeId: `org.jetbrains.kotlin:kotlin-compiler-embeddable:jar@${kotlinVersion}`,
+      },
+      {
+        nodeId: `org.jetbrains.kotlin:kotlin-scripting-compiler-embeddable:jar@${kotlinVersion}`,
+      },
+      {
+        nodeId: `org.jetbrains.kotlin:kotlin-allopen-compiler-plugin-embeddable:jar@${kotlinVersion}`,
+      },
+      {
+        nodeId: `org.jetbrains.kotlin:kotlin-noarg-compiler-plugin-embeddable:jar@${kotlinVersion}`,
+      },
+      { nodeId: `org.jetbrains.kotlin:abi-tools:jar@${kotlinVersion}` },
+      {
+        nodeId: `org.jetbrains.kotlin:kotlin-klib-commonizer-embeddable:jar@${kotlinVersion}`,
+      },
+      { nodeId: `org.jetbrains.kotlin:kotlin-reflect:jar@${kotlinVersion}` },
+    ];
+
+    expectedDirectDependencies.forEach((expectedDependency) => {
+      expect(directDependencies).toContainEqual(expectedDependency);
+    });
+  }, 200000);
 }
